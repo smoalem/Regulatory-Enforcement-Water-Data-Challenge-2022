75cc441a7bacc0bfedfed135ef23d1913d9fc099
jahongOf1 <jheafpp@gmail.com>
2022-09-30 17:37:56 -0500

    adding label and trying to add spectrum of color

--- rprog/open_map_example.R
+++ rprog/open_map_example.R
@@ -12,64 +12,70 @@ library(jsonlite)
 library(rgeos)
 #
 
-url <- "C:/Users/user/Documents/GitHub/Regulatory-Enforcement-Water-Data-Challenge-2022/new_export_test.geojson"
- 
-
+url <- "C:/Users/jheaf/Documents/Regulatory-Enforcement-Water-Data-Challenge-2022/new_export_test.geojson"
 res <- readOGR(dsn = url, layer="new_export_test")
-poly <- res[, 0]@polygons
 centers <- data.frame(gCentroid(res, byid = TRUE))
+res$lng <- centers$x
+res$lat <- centers$y
 regular_data <- data.frame(res[, c(2,15, 35)])
 
+#
 # #
-# ui <- dashboardPage(
-#   dashboardHeader(
-#     title="TEST"
-#   ),
-#   dashboardSidebar(
-#     width = 350
-#   ),
-#   dashboardBody(
-# 
-#     tabsetPanel(
-#       type = "tabs",
-#       id = "tab_selected",
-#       tabPanel(
-#         title = "Map",
-#         leafletOutput("camap")
-# 
-#       ),
-#       tabPanel(
-#         title = "Data"
-#       ),
-#       tabPanel(
-#         title = "FAQ"
-# 
-#       )
-# 
-#     )
-#   )
-# 
-# 
-# 
-# )
+ui <- dashboardPage(
+  dashboardHeader(
+    title="TEST"
+  ),
+  dashboardSidebar(
+    width = 350
+  ),
+  dashboardBody(
+
+    tabsetPanel(
+      type = "tabs",
+      id = "tab_selected",
+      tabPanel(
+        title = "Map",
+        leafletOutput("camap")
+
+      ),
+      tabPanel(
+        title = "Data"
+      ),
+      tabPanel(
+        title = "FAQ"
+
+      )
+
+    )
+  )
+
+
+
+)
 # #
 # # # Server
-# #
-# server <- function(input, output) {
-# 
-#   map <- leaflet(poly) %>%
-#       addTiles() %>%
-#       addPolygons(weight=2, col = "red", fillColor = "yellow") %>%
-#       setView(lng = -119.417931, lat = 36.778259, zoom = 5)
-# 
-# 
-#   output$camap <- renderLeaflet(map)
-# # #
-# # #
-# # # }
-# #
-# #
-# #
-# # # Shiny App
-# #
-# shinyApp(ui, server)
+#
+server <- function(input, output) {
+  popup <- 
+  map <- leaflet(res) %>%
+      addTiles() %>%
+      addPolygons(weight=2, col = "red", popup = ~paste0(
+        "<b> Water System Number: <b>", 
+        WATER_SYST,
+        "<br>",
+        "<b> Water System Name: <b>", 
+        WATER_SY_1
+        ), fillColor = "yellow") %>%
+      setView(lng = -119.417931, lat = 36.778259, zoom = 5)
+
+
+  output$camap <- renderLeaflet(map)
+
+
+}
+
+
+#
+# # Shiny App
+#
+shinyApp(ui, server)

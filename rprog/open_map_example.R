98b0aaef0df74d2e748523a3c349a8c55040bd68
jahongOf1 <40527955+jahongOf1@users.noreply.github.com>
2022-09-30 12:43:30 -0500

    map example work in centroid function and now have the ability to separate geojson variables

--- rprog/open_map_example.R
+++ rprog/open_map_example.R
@@ -1,3 +1,5 @@
+# Libs
+
 library(shiny)
 library(shinydashboard)
 library(shinyjs)
@@ -7,19 +9,67 @@ library(rgdal)
 library(magrittr) # needs to be run every time you start R and want to use %>%
 library(dplyr)    # alternatively, this also loads %>%
 library(jsonlite)
+library(rgeos)
+#
 
-# url <- "C:/Users/jheaf/Dropbox/Water Data Challenge 2022/GIS/water_system_with_scores.geojson"
 url <- "C:/Users/user/Documents/GitHub/Regulatory-Enforcement-Water-Data-Challenge-2022/new_export_test.geojson"
+ 
 
-res <- readOGR(dsn = url)
-# geojson <- jsonLite::readLines(url) %>%
-#   paste(collapse = "\n") %>%
-#   fromJSON(simplifyVector = FALSE)
+res <- readOGR(dsn = url, layer="new_export_test")
+poly <- res[, 0]@polygons
+centers <- data.frame(gCentroid(res, byid = TRUE))
+regular_data <- data.frame(res[, c(2,15, 35)])
 
-
-
-leaflet(res) %>% 
-  addTiles() %>%
-  addPolylines(fillColor = "red") %>%
-  addPolygons(fillColor = "yellow") %>%
-  setView(lng = -119.417931, lat = 36.778259, zoom = 5)
+# #
+# ui <- dashboardPage(
+#   dashboardHeader(
+#     title="TEST"
+#   ),
+#   dashboardSidebar(
+#     width = 350
+#   ),
+#   dashboardBody(
+# 
+#     tabsetPanel(
+#       type = "tabs",
+#       id = "tab_selected",
+#       tabPanel(
+#         title = "Map",
+#         leafletOutput("camap")
+# 
+#       ),
+#       tabPanel(
+#         title = "Data"
+#       ),
+#       tabPanel(
+#         title = "FAQ"
+# 
+#       )
+# 
+#     )
+#   )
+# 
+# 
+# 
+# )
+# #
+# # # Server
+# #
+# server <- function(input, output) {
+# 
+#   map <- leaflet(poly) %>%
+#       addTiles() %>%
+#       addPolygons(weight=2, col = "red", fillColor = "yellow") %>%
+#       setView(lng = -119.417931, lat = 36.778259, zoom = 5)
+# 
+# 
+#   output$camap <- renderLeaflet(map)
+# # #
+# # #
+# # # }
+# #
+# #
+# #
+# # # Shiny App
+# #
+# shinyApp(ui, server)

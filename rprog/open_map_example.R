8b6e489ca5778a76c4ffaf82935e697bef9825a9
jahongOf1 <jheafpp@gmail.com>
2022-10-01 03:56:35 -0500

    new geojson and map mostly complete

--- rprog/open_map_example.R
+++ rprog/open_map_example.R
@@ -12,21 +12,30 @@ library(jsonlite)
 library(rgeos)
 #
 
-url <- "C:/Users/jheaf/Documents/Regulatory-Enforcement-Water-Data-Challenge-2022/new_export_test.geojson"
-res <- readOGR(dsn = url, layer="new_export_test")
-centers <- data.frame(gCentroid(res, byid = TRUE))
-res$lng <- centers$x
-res$lat <- centers$y
-regular_data <- data.frame(res[, c(2,15, 35)])
+url <- "C:/Users/jheaf/Documents/Regulatory-Enforcement-Water-Data-Challenge-2022/removed_neg_scores.geojson"
+res <- readOGR(dsn = url, layer="removed_neg_scores")
+# centers <- data.frame(gCentroid(res, byid = TRUE))
+# res$lng <- centers$x
+# res$lat <- centers$y
+pal <- colorNumeric(
+  palette = "Blues",
+  domain = res$red_int
+)
+# 
 
-#
+binpal <- colorBin("Blues", res$red_int, 10, pretty = FALSE)
+
+
+# #
 # #
 ui <- dashboardPage(
   dashboardHeader(
     title="TEST"
   ),
   dashboardSidebar(
-    width = 350
+    width = 350,
+    uiOutput("sidebar")
+    
   ),
   dashboardBody(
 
@@ -52,25 +61,52 @@ ui <- dashboardPage(
 
 
 )
-# #
-# # # Server
-#
+# Server
+
 server <- function(input, output) {
-  popup <- 
+### Map 
   map <- leaflet(res) %>%
       addTiles() %>%
-      addPolygons(weight=2, col = "red", popup = ~paste0(
-        "<b> Water System Number: <b>", 
-        WATER_SYST,
-        "<br>",
-        "<b> Water System Name: <b>", 
-        WATER_SY_1
-        ), fillColor = "yellow") %>%
+      addPolygons(
+        stroke = FALSE,
+        smoothFactor = 0.2,
+        fillOpacity = 0.9,
+        dashArray = "3",
+        color = ~binpal(red_int),
+        popup = ~paste0(
+          "<b> Water System Number: <b>",
+          WATER_SYST,
+          "<br>",
+          "<b> Water System Name: <b>",
+          WATER_SY_1
+        )
+        ) %>%
+      addLegend("bottomright", pal = binpal, values = ~red_int,
+                title = "Legend",
+                opacity = 1
+      ) %>%
       setView(lng = -119.417931, lat = 36.778259, zoom = 5)
 
 
   output$camap <- renderLeaflet(map)
-
+### Render UI
+  output$complianceOverage <- renderUI({
+    selectInput(
+      inputId = "complianceOverage", 
+      label = strong("Select Score Type:", style = "font-family: 'arial'; font-si28pt"),
+      choices =  c("Compliance Score", "Overage Score"),
+      selected = choices[1]
+    )
+  })
+  
+  output$sidebar <- renderUI({
+    if( input$tab_selected == "Map"){
+      div(
+        uiOutput("complianceOverage")
+      )
+    } 
+    
+  })
 
 }
 
